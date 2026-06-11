e56de7db05ea2f9fc13ec8d691cbe514043f9035
Peter Wilkins <poppetew@gmail.com>
2026-06-11 21:15:50 +0100

    #112 app-level broken-app fallback boundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrokenAppBoundary } from './BrokenAppBoundary.jsx'
 import './index.css'
 import { registerServiceWorker } from './services/serviceWorker.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrokenAppBoundary>
+      <App />
+    </BrokenAppBoundary>
   </React.StrictMode>,
 )
 
